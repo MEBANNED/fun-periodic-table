f866936b5529f324e3eafc955aa55b9dbb25d2f5
MEBANNED <frankchencty@gmail.com>
2025-07-05 21:56:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))  # Replit uses dynamic port
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # Render and Replit both respect PORT env, default to 3000 locally
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
