730f54f61815ff0ebb02e1b4fb532564bfa27dc2
MEBANNED <frankchencty@gmail.com>
2025-07-05 22:58:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Render and Replit both respect PORT env, default to 3000 locally
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
