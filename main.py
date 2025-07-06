ce5af71116646ed4ccae5452437054b63ce6e402
MEBANNED <frankchencty@gmail.com>
2025-07-06 13:47:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # looks in templates/
 
 if __name__ == "__main__":
+    # Render / Replit set an env var called PORT.
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
